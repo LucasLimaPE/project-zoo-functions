02cc42fd0af49e58aea348d0f1742ef7d7b1f7c5
LucasLimaPE <robertolucas011@gmail.com>
2022-01-29 14:50:06 -0300

    Adiciona requisito 1.

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,15 @@
+const { species } = require('../data/zoo_data');
+const zoo_data = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-  
+function getSpeciesByIds(...args) {
+  if (args.length === undefined) {
+    return [];
+  }
+  if (args.length === 1) {
+    return species.filter((animal) => animal.id === args[0]);
+  }
+  return species.filter((element, index) => element.id === args[index]);
 }
 
 module.exports = getSpeciesByIds;
