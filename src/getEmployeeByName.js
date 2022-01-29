58e0b66271c336ad6c4401f390b7c8ec78a5da4e
LucasLimaPE <robertolucas011@gmail.com>
2022-01-29 16:18:36 -0300

    Adiciona requisito 3.

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  if (employeeName !== undefined) {
+    const result = employees.filter((employee) => employee.firstName === employeeName
+   || employee.lastName === employeeName);
+    return result[0];
+  }
 }
 
 module.exports = getEmployeeByName;
