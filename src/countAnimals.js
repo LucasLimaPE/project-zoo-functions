8314d628b08e97f0ce464ef29f4945fa9cd832ed
LucasLimaPE <robertolucas011@gmail.com>
2022-01-30 19:55:02 -0300

    Adiciona requisito 5.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+/*
+ Este código foi desenvolvido com o auxílio do colega Guilherme Azevedo, após várias tentativas de fazê-lo ele me explicou a questão do reduce e da negação usada no segundo if.
+*/
 
+function countAnimals(animal) {
+  if (animal === undefined) {
+    return species.reduce(((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }), {});
+  } if (animal.specie && !animal.sex) {
+    return species.find((element) => element.name === animal.specie).residents.length;
+  }
+  return species.find((element) => element.name === animal.specie)
+    .residents.filter((element) => element.sex === animal.sex).length;
+}
 module.exports = countAnimals;
