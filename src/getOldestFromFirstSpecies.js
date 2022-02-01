2200ec8311d46917e9f13277b729778f78392479
LucasLimaPE <robertolucas011@gmail.com>
2022-02-01 18:58:23 -0300

    Adiciona requisito 9.

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,19 @@
+/*
+Este código foi desenvolvido com a ajuda do colega Guilherme Azevedo.
+*/
+
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employee = employees.find((element) => element.id === id);
+  const animalId = employee.responsibleFor[0];
+  const animals = species.find((element) => element.id === animalId);
+  const oldestAnimal = animals.residents.reduce((acc, current) => {
+    if (current.age > acc.age) return current;
+    return acc;
+  });
+  return Object.values(oldestAnimal);
 }
 
 module.exports = getOldestFromFirstSpecies;
