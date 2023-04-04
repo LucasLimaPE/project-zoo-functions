638053e1c61e4a87041aad63c73b1646318f2be1
LucasLimaPE <robertolucas011@gmail.com>
2023-04-04 20:29:13 -0300

    refactor: decreases the complexity of the getSpeciesByIds function.

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,15 +1,7 @@
 const { species } = require('../data/zoo_data');
-// const zoo_data = require('../data/zoo_data');
-const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...args) {
-  if (args.length === undefined) {
-    return [];
-  }
-  if (args.length === 1) {
-    return species.filter((animal) => animal.id === args[0]);
-  }
-  return species.filter((element, index) => element.id === args[index]);
+  return args.length === 0 ? [] : species.filter((animal) => args.includes(animal.id));
 }
 
 module.exports = getSpeciesByIds;
