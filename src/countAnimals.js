aa3983167b7c55e8d19f48ea055eb2454d1d5b5b
LucasLimaPE <robertolucas011@gmail.com>
2023-04-04 22:43:21 -0300

    refactor: separates the responsibilities in the countAnimals function code, reducing complexity and improving writing.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,20 +1,31 @@
 const { species } = require('../data/zoo_data');
-const data = require('../data/zoo_data');
 
 /*
  Este código foi desenvolvido com o auxílio do colega Guilherme Azevedo, após várias tentativas de fazê-lo ele me explicou a questão do reduce e da negação usada no segundo if.
 */
 
-function countAnimals(animal) {
-  if (animal === undefined) {
-    return species.reduce(((acc, curr) => {
-      acc[curr.name] = curr.residents.length;
-      return acc;
-    }), {});
-  } if (animal.specie && !animal.sex) {
-    return species.find((element) => element.name === animal.specie).residents.length;
+const countTotalAnimals = () =>
+  species.reduce((acc, curr) => {
+    acc[curr.name] = curr.residents.length;
+    return acc;
+  }, {});
+
+const countAnimalWithSpecie = ({ specie }) =>
+  species.filter((item) => item.name === specie)[0].residents.length;
+
+const countAnimalWithSex = ({ specie, sex }) =>
+  species
+    .filter((item) => item.name === specie)[0]
+    .residents.filter((secondItem) => secondItem.sex === sex).length;
+
+function countAnimals(object) {
+  if (!object) {
+    return countTotalAnimals();
   }
-  return species.find((element) => element.name === animal.specie)
-    .residents.filter((element) => element.sex === animal.sex).length;
+  if (!object.sex) {
+    return countAnimalWithSpecie(object);
+  }
+  return countAnimalWithSex(object);
 }
+
 module.exports = countAnimals;
