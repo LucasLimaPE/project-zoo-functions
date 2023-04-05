c8d3f4f133e3418b9cfeae917a293dbdfa0c5630
LucasLimaPE <robertolucas011@gmail.com>
2023-04-04 21:28:50 -0300

    refactor: decreases code complexity and improves getRelatedEmployees fuction writing.

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,21 +1,19 @@
 const { employees } = require('../data/zoo_data');
-const data = require('../data/zoo_data');
 
 function isManager(id) {
-  if (id === '9e7d4524-363c-416a-8759-8aa7e50c0992'
-  || id === 'fdb2543b-5662-46a7-badc-93d960fdc0a8'
-  || id === '0e7b460e-acf4-4e17-bcb3-ee472265db83'
-  || id === 'b0dc644a-5335-489b-8a2c-4e086c7819a2') {
-    return true;
-  } return false;
+  return employees.some((item) => item.managers.includes(id));
 }
 
+const throwError = () => {
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+};
+
 function getRelatedEmployees(managerId) {
-  if (isManager(managerId) === false) {
-    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
-  }
-  return employees.filter((employee) => employee.managers.includes(managerId)).map((employee2) =>
-    `${employee2.firstName} ${employee2.lastName}`);
+  const maneged = employees
+    .filter((employee) => employee.managers.includes(managerId))
+    .map((employee2) => `${employee2.firstName} ${employee2.lastName}`);
+
+  return isManager(managerId) ? maneged : throwError();
 }
 
 module.exports = { isManager, getRelatedEmployees };
