2ddc5dffb18fee3ccb7bb2513d4d7dbc7a8a56b9
LucasLimaPE <robertolucas011@gmail.com>
2023-04-14 17:41:46 -0300

    feat: implements the getAnimalMap function.

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,55 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+const getAllAnimalsMap = () => {
+  const result = species.reduce((acc, curr) => {
+    acc[curr.location] = species
+      .filter((item) => item.location === curr.location)
+      .map((item) => item.name);
+    return acc;
+  }, {});
+
+  return result;
+};
+
+function filterAnimalsBySex(animals, sex) {
+  return animals.filter((animal) => animal.sex === sex);
+}
+
+function sortAnimalsByName(animals) {
+  return animals.sort();
+}
+
+function mapAnimalsToNames(animals) {
+  return animals.map((animal) => animal.name);
+}
+
+function getAllAnimalsMapWithName(...params) {
+  const { includeNames, sex, sorted } = params[0] || {};
+  const animalMap = species.reduce((accumulator, currentSpecies) => {
+    let animals = currentSpecies.residents;
+    if (sex) { animals = filterAnimalsBySex(animals, sex); }
+    if (includeNames) {
+      animals = mapAnimalsToNames(animals);
+    } else {
+      animals = getAllAnimalsMap();
+    }
+    if (sorted) {
+      animals = sortAnimalsByName(animals);
+    }
+    const { location, name } = currentSpecies;
+    accumulator[location] = [...(accumulator[location] || []), { [name]: animals }];
+    return accumulator;
+  }, {});
+
+  return animalMap;
+}
+
+function getAnimalMap(...params) {
+  if (params.length === 0 || !params[0].includeNames) {
+    return getAllAnimalsMap();
+  }
+  return getAllAnimalsMapWithName(...params);
 }
 
 module.exports = getAnimalMap;
