4301a996415f7b926b2a6b7fc7cd27193f0ceeea
LucasLimaPE <robertolucas011@gmail.com>
2023-04-15 10:28:53 -0300

    refactor: changes the form that recovers the older animal.

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -9,11 +9,8 @@ function getOldestFromFirstSpecies(id) {
   const employee = employees.find((element) => element.id === id);
   const animalId = employee.responsibleFor[0];
   const animals = species.find((element) => element.id === animalId);
-  const oldestAnimal = animals.residents.reduce((acc, current) => {
-    if (current.age > acc.age) return current;
-    return acc;
-  });
-  return Object.values(oldestAnimal);
+  const olderAnimal = animals.residents.sort((a, b) => b.age - a.age)[0];
+  return Object.values(olderAnimal);
 }
 
 module.exports = getOldestFromFirstSpecies;
